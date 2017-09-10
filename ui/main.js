0a3827c1b48e7e50ccd7b5a6124d419417a3a442
drmaniak <manick.vennimalai@gmail.com>
2017-09-10 19:53:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+function main(){
+    $('.bold').hide();
+    $('.bold').fadeIn(500);
+    $('.second').hide();
+    $('.bold').on('click', function (){
+        $(this).next().slideToggle(500);
+        $(this).text("Shazam!!");
+    })
+    $(document).ready(main);
+}
