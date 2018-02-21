abcf2f357d671776898e4b350fe0bc48c40cad51
drmaniak <manick.vennimalai@gmail.com>
2018-02-21 14:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,17 @@
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function() {
+    // Make a request to counter endpoint
+    
+    // Capture the response and store it in a variable
+    
+    //  Render the variable in the correct span
+    counter += 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString;
+};
+
 var main= function(){
     
     
