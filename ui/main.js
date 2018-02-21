9905586288144697068eca00c970fe1217335306
drmaniak <manick.vennimalai@gmail.com>
2018-02-21 14:03:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,30 @@
 var main= function(){
-    var button = $("#counter");
-    button.click(function(){
-        //Create a request object
-        var request =new XMLHttpRequest();
-        //capture a response and store it in a variable
-        request.onreadystatechange = function() {
-            if(request.readyState === XMLHttpRequest.DONE){
-                //take action
-                if(request.status === 200){
-                    var counter = request.responseText;
-                    var span = $("#count"); 
-                    span.html(counter);
-                }
-            }
-        };
+    
+    
+    
+    
+    
+    // var button = $("#counter");
+    // button.click(function(){
+    //     //Create a request object
+    //     var request =new XMLHttpRequest();
+    //     //capture a response and store it in a variable
+    //     request.onreadystatechange = function() {
+    //         if(request.readyState === XMLHttpRequest.DONE){
+    //             //take action
+    //             if(request.status === 200){
+    //                 var counter = request.responseText;
+    //                 var span = $("#count"); 
+    //                 span.html(counter);
+    //             }
+    //         }
+    //     };
         
-        //Make a request
-        request.open('GET', 'http://manickvennimalai.imad.hasura-app.io/counter', true);
-        request.send(null);        
+    //     //Make a request
+    //     request.open('GET', 'http://manickvennimalai.imad.hasura-app.io/counter', true);
+    //     request.send(null);        
             
-    });
+    // });
     //submit name
     
     var submit = $("#submit_btn");
