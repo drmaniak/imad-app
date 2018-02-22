05b05f0f74ac268c21da4543aa337e4eac339732
drmaniak <manick.vennimalai@gmail.com>
2018-02-22 17:05:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -102,7 +102,7 @@ var main= function(){
            }
        };
        var nameList = $("#name");
-       var name = nameList.value;
+       var name = nameList.val();
        
        request.open("GET", "http://manickvennimalai.imad.hasura-app.io/submit-name?name="+ name, true);
        request.send(null);
