9345ffb12eb0579f83a3cb5f4707c2c40d9f9bdf
drmaniak <manick.vennimalai@gmail.com>
2018-02-22 15:04:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -178,9 +178,9 @@ app.get('/comments', function(req, res){
 // });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) {
     //Get the name from the request
-    var name = req.params.name; 
+    var name = req.query.name; 
     
     names.push(name);
     
