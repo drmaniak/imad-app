df4e3d18bfd85f6483d4fb16f80ac0c28b325f3f
drmaniak <manick.vennimalai@gmail.com>
2018-03-01 14:20:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,14 +150,15 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
+
 app.get('/test-db', function(req, res) {
     //make a select request
     // return a response with the resutl
-    pool.query('SELECT * FROM test', function(err, result) {
+    pool.query('SELECT * FROM test', function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         }else {
-            res.send(JSON.stringify(results));
+            res.send(JSON.stringify(result));
         }
     });
     
