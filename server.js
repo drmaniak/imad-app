e5a8656bbed2adc7012d8597194df04c76a1b46a
drmaniak <manick.vennimalai@gmail.com>
2018-03-05 13:03:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.get('/submit-name', function(req, res) {
 app.get('/articles/:articleName', function(req, res){
     // articleName == article-one to article-six
     // articles[articleName] == selected {} object based on url
-    pool.query(" SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
+    pool.query(" SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result) {
        if (err) {
            res.status(500).send(err.toString());
        } else {
