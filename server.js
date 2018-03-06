94b297e5bbb9c95f239eeb883cd81fb06c3c5b50
drmaniak <manick.vennimalai@gmail.com>
2018-03-06 20:21:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,6 +145,11 @@ app.get("/check-login", function(req, res) {
    }
 });
 
+app.get('/logout', function(req, res) {
+    delete req.session.auth;
+    res.send("You have been logged out");
+});
+
 var pool = new Pool(config);
 
 app.get('/test-db', function(req, res) {
