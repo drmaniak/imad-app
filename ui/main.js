eb254b064424760ba66a8f29502dde245352bd49
drmaniak <manick.vennimalai@gmail.com>
2018-03-06 21:07:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,6 @@
-var main= function(){
-    // Submit username/password
-    
-    var submit = $("#submit_btn");
-    submit.click(function() {
-      var request = new XMLHttpRequest();
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
       request.onreadystatechange = function(){
           if (request.readyState === 4) {
               if (request.status === 200) {
@@ -16,14 +13,43 @@ var main= function(){
               }
           }
       };
-      var username = $("#username").val();
-      var password = $("#password").val();
-      console.log("Username: " + username);
-      console.log("Password: " + password);
-      request.open("POST", "http://manickvennimalai.imad.hasura-app.io/login", true);
-      request.setRequestHeader('Content-Type', 'application/json');
-      request.send(JSON.stringify({username: username, password: password }));
-    });    
+        var username = document.getElementById('username');
+        var password = document.getElementById('password');
+        console.log("Username: " + username);
+        console.log("Password: " + password);
+        request.open("POST","http://manickvennimalai.imad.hasura-app.io/login", true );
+        requst.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));
+};
+
+
+
+var main= function(){
+    // // Submit username/password
+    
+    // var submit = $("#submit_btn");
+    // submit.click(function() {
+    //   var request = new XMLHttpRequest();
+    //   request.onreadystatechange = function(){
+    //       if (request.readyState === 4) {
+    //           if (request.status === 200) {
+    //                 console.log("Logged in successfully");
+    //                 alert("Logged in successfully");
+    //           }else if (request.status === 403) {
+    //                 alert("Invalid username/password");
+    //           }else if (request.status === 500) {
+    //                 alert("Something went wrong on the server");
+    //           }
+    //       }
+    //   };
+    //   var username = $("#username").val();
+    //   var password = $("#password").val();
+    //   console.log("Username: " + username);
+    //   console.log("Password: " + password);
+    //   request.open("POST", "http://manickvennimalai.imad.hasura-app.io/login", true);
+    //   request.setRequestHeader('Content-Type', 'application/json');
+    //   request.send(JSON.stringify({username: username, password: password }));
+    // });    
     
     
     // // COUNTER CODE JQUERY
