2131a393e6e2f99d0603fbcc5a9f05507dfaf709
drmaniak <manick.vennimalai@gmail.com>
2018-03-06 20:03:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,9 @@ app.post('/login' ,  function(req, res) {
 app.get("/check-login", function(req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
        res.send('User is currently logged in: ' + req.session.auth.userId.toString());
-   } 
+   } else {
+       res.send('You are not logged in.');
+   }
 });
 
 var pool = new Pool(config);
