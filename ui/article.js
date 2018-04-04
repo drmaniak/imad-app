0010354d2696dbcc6d43eed4aa34c446a9f20b19
drmaniak <manick.vennimalai@gmail.com>
2018-04-04 14:36:27 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -10,6 +10,64 @@ function loadCommentForm() {
         <button type="submit" id="submit" class="btn-primary btn-sm">Submit</button>
         <br/>`;
         
-        document.getElementById('comment_form').innerHTML = commentFormHtml;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
         
+    //Submit username/password to login
+    var submit = document.getElementById('submit');
+    
+    submit.onclick = function() {
+      var request = new XMLHttpRequest();
+      
+      request.onReadyStateChange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action 
+            if (request.status === 200) {
+                document.getElementById('comment_text').value = '';
+                loadComments();
+            } else {
+                alert('Error, could not submit comment');
+            }
+            submit.value = 'Submit';
+        }  
+      };
+      
+      var comment = document.getElementById('comment_text').value;
+      request.open('POST', '/submit-comment' + currentArticleTitle, true);
+      request.setRequestHeader('Content-type', 'application/json');
+      request.send(JSON.stringify({comment: comment}));
+      submit.value = 'Submitting';
+    };
+        
+}
+
+function loadLogin() {
+    var request = new XMLHttpRequest();
+    
+    request.onReadyStateChange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.resonseText);
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+function escapeHTML (text) {
+    var $text = document.createTextNode(text);
+    var $div = document.createElement('div');
+    $div.appendChild($text);
+    return $div.innerHTML;
+}
+
+function loadComments () {
+    var request = new XMLHttpRequest();
+    request.onReadyStateChange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          var comments = document.getElementById('comments')
+          
+      }  
+    };
 }
