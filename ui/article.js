25cebf81f012acfd2d588850194596ec8d59b607
drmaniak <manick.vennimalai@gmail.com>
2018-04-04 14:00:17 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,14 @@
+// Eg: coco98.imad.hasura-app.io/articles/article-one will result in article-one
+var currentArticleTitle = window.location.pathname.split('/')[2];
 
+
+function loadCommentForm() {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <button type="submit" class="btn-primary btn-sm">Submit</button>
+        <br/>`;
+        
+        document.getElementById('comment_form').innerHTML = commentFormHtml;
+}
