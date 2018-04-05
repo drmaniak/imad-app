09733bb5c5615093ae30a72f87fe8b6205474d48
drmaniak <manick.vennimalai@gmail.com>
2018-04-06 00:58:50 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -76,7 +76,7 @@ function loadComments () {
               for( var i = 0; i < commentsData.length; i++) {
                   var time = new Date(commentsData[i].timestamp);
                   content += `
-                  <div class = "comment well well-lg">
+                  <div class = "comment">
                     <p class = "well well-lg">${escapeHTML(commentsData[i].comment)}</p>
                     <div class = "commenter">
                         Posted by: <i>${commentsData[i].username}</i> 
